7503ab18846c68446f7d509a8e155b1637afbc84
Nick <namp@kth.se>
2017-02-23 20:46:55 +0100

    bugs fixing

--- hash2017.java
+++ hash2017.java
@@ -6,15 +6,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
-public static int NO_OF_VIDEO;
-public static int NO_OF_ENDPOINT;
-public static int NO_OF_SERVER;
-public static int SERVER_CAPACITY;
-public static Video[] videos;
-
-
-
 public class hash2017 {
     public static void main(String[] args){
         System.out.println("Hello Hashcode");
@@ -23,14 +14,20 @@ public class hash2017 {
         stream_input(file_path);
     }
 
+    public static int NO_OF_VIDEO;
+    public static int NO_OF_ENDPOINT;
+    public static int NO_OF_SERVER;
+    public static int SERVER_CAPACITY;
+    public static Video[] videos;
+
     public static void stream_input(String file_path){
       try{
         BufferedReader br = new BufferedReader(new FileReader(file_path));
-        String line1 = br.readLine(); //reading the first line
+        String firstLine = br.readLine(); //reading the first line
 
         // Get four arguments (int)
 
-        int[] Arguments = Arrays.asList(line1.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+        int[] Arguments = Arrays.asList(firstLine.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
         NO_OF_VIDEO = Arguments[0];
         NO_OF_ENDPOINT = Arguments[1];
         NO_OF_SERVER = Arguments[2];
@@ -38,18 +35,16 @@ public class hash2017 {
         videos = new Video[NO_OF_VIDEO];
 
 
-	String Line1 = br.readLine();
-	Arguments = Arrays.asList(line1.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+  String rest = br.readLine();
+	Arguments = Arrays.asList(rest.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
 	for(int i = 0; i<NO_OF_VIDEO ; i++){
 	    videos[i] = new Video(i,Arguments[i]);
 	}
-
 	
-
-	
-        while((line = br.readLine()) != null){
+      //do operations to everyline after line 1 here
+        while((rest = br.readLine()) != null){
           System.out.println("undecided");
-          }
+        }
 
         } catch (FileNotFoundException e) {
         e.printStackTrace();
