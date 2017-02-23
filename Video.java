cc9d8d454c292b4395f7f07634445478ee2edee9
Nick <namp@kth.se>
2017-02-23 20:30:51 +0100

    what is this

---
+++ Video.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Video {
+  int num;
+  int size;
+  HashMap<Integer,Integer> request = new HashMap<>();
+
+  public void videoInfo(int num, int size, HashMap<Integer,Integer> request) {
+    this.num = num;
+    this.size = size;
+    this.request = request;
+  }
+}
