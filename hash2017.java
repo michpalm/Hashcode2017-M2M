8ad683e1b2b79bdc5e55c055d2f9ead06ff08e72
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-23 21:41:34 +0100

    renamed i to k

--- hash2017.java
+++ hash2017.java
@@ -41,12 +41,12 @@ public class hash2017 {
 
 	    String rest = br.readLine();
 	    Arguments = Arrays.asList(rest.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
-	    for(int i = 0; i<NO_OF_VIDEO ; i++){
-		if (Arugments[i] > SERVER_CAPACITY) {
+	    for(int k = 0; k<NO_OF_VIDEO ; k++){
+		if (Arguments[k] > SERVER_CAPACITY) {
 		    //to filter out big videos
-		    video[i] = null;
+		    video[k] = null;
 		}else{
-		    videos[i] = new Video(i,Arguments[i]);
+		    videos[k] = new Video(k,Arguments[k]);
 
 		}
 		endpoints = new Endpoint[NO_OF_ENDPOINT];
