07b8cef5f154d137a1a2eaa08b615b3077328fa2
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-23 19:39:08 +0100

    Parsing the first line

--- hash2017.java
+++ hash2017.java
@@ -1,6 +1,38 @@
+import java.util.HashMap;
 
 public class hash2017 {
     public static void main(String[] args){
         System.out.println("Hello Hashcode");
+        String file_path = "input/me_at_the_zoo.in"
+
+        stream_input(file_path);
     }
+
+    public static void stream_input(String file_path){
+      try{
+        BufferedReader br = new BufferedReader(new FileReader(file_path));
+        String line;
+        String line1 = br.readLine(); //reading the first line
+
+        // Get four arguments (int)
+        public int[] Arguments = Arrays.asList(line1.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+
+        System.out.println("Arguments is here:  " + Arguments);
+        while((line = br.readLine()) != null){
+          System.out.println("undecided");
+          }
+
+        } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+
+        }
+}
+
+public class videoInfo {
+  int videoName;
+  int size;
+  HashMap<Integer,Integer> request = new HashMap<>();
 }
