f75227f77a81003354d5fb2e3c200c66f54f1205
Nick <namp@kth.se>
2017-02-23 20:15:24 +0100

    videoInfo class

---
+++ videoInfo.java
@@ -0,0 +1,13 @@
+public class videoInfo {
+  int num;
+  int size;
+  HashMap<Integer,Integer> request = new HashMap<>();
+
+  public videoInfo(int num, int size, HashMap<Interger,Interger> request {
+    this.num = num;
+    this.size = size;
+    this.request = request;
+  }
+
+
+}
